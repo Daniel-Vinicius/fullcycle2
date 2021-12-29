fee15b1c63ece49d528977533d0384edd37d98fd
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-12-29 18:08:10 -0300

    NodeJS App com DB no Docker

--- docker/course_docker/node/index.js
+++ docker/course_docker/node/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+const mysql = require('mysql')
 
-app.get('/', (req, res) => {
-  return res.send('Hello World')
+app.get('/:name', (req, res) => {
+  const name = req.params.name;
+  const connection = mysql.createConnection(config)
+  
+  const sql = `INSERT INTO people(name) values('${name}')`
+  connection.query(sql)
+  connection.end()
+  return res.send(`${name} inserted on MySQL database`)
 })
 
 app.listen(port, () => {
