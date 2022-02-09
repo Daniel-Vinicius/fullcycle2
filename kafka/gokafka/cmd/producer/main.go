11fcfac23e5bbcfaad50a56743844fec5851f4c6
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-02-09 20:04:12 -0300

    Publicando primeira mensagem

--- kafka/gokafka/cmd/producer/main.go
+++ kafka/gokafka/cmd/producer/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
-	fmt.Println("Hello Go")
+	producer := NewKafkaProducer()
+	Publish("Hello Go with Kafka", "teste", producer, nil)
+
+	// Para o programa Go não morrer antes de dar tempo de mandar a mensagem
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "gokafka_kafka_1:9092",
+	}
+
+	producer, err := kafka.NewProducer(configMap)
+
+	if err != nil {
+		log.Println("Failed to create producer:", err.Error())
+	}
+
+	return producer
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- kafka/gokafka/go.mod
+++ kafka/gokafka/go.mod
@@ -1,3 +1,5 @@
 module github.com/Daniel-Vinicius/fullcycle2/tree/master/kafka/gokafka
 
 go 1.17
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
