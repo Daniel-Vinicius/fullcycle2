3919da8381f5a1a309eb5698e9ad3e292c641912
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-04-01 20:37:41 -0300

    Princípio da autovalidação

--- DDD/src/entity/customer.ts
+++ DDD/src/entity/customer.ts
@@ -1,20 +1,39 @@
 class Customer {
 	private _id: string;
 	private _name: string;
-	private _address: string;
-	private _active = true;
+	private _address = "";
+	private _active = false;
 
-	constructor(id: string, name: string, address: string) {
+	constructor(id: string, name: string) {
 		this._id = id;
 		this._name = name;
-		this._address = address;
+		this.validate();
+	}
+
+	validate() {
+		if (this._name.length === 0) {
+			throw new Error("Name is required");
+		}
+
+		if (this._name.split(" ").length < 2) {
+			throw new Error("Name must contain at least two words");
+		}
+
+		if (this._id.length === 0) {
+			throw new Error("Id is required");
+		}
 	}
 
 	changeName(name: string) {
 		this._name = name;
+		this.validate();
 	}
 
 	activate() {
+		if (this._address.length === 0) {
+			throw new Error("Address is mandatory to active a customer");
+		}
+
 		this._active = true;
 	}
 
@@ -22,3 +41,6 @@ class Customer {
 		this._active = false;
 	}
 }
+
+const customer = new Customer("1", "John Doe");
+customer.activate();
