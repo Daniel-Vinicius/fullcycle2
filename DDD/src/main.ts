3b7fca47ba5797cc23fd51f7df09778e9a02bf67
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-04-07 19:15:18 -0300

    Jest vs SWC - Check Types on Test Command

--- DDD/src/main.ts
+++ DDD/src/main.ts
@@ -10,8 +10,8 @@ const address = new Address("Wilkie Way", 4290, 94306, "Palo Alto, CA");
 customer.setAddress(address);
 customer.activate();
 
-const item1 = new OrderItem("1", "Shirt", 99.99);
-const item2 = new OrderItem("2", "Shoes", 199.99);
+const item1 = new OrderItem("1", "Shirt", 99.99, "product-1", 2);
+const item2 = new OrderItem("2", "Shoes", 199.99, "product-2", 1);
 
 const order = new Order("1", customer.id, [item1, item2]);
 
