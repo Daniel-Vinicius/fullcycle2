e2efa339b4de6b8a0244807ace0a0866278ef9b9
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-04-07 18:35:36 -0300

    Criando Classe Product

---
+++ DDD/src/entity/product.ts
@@ -0,0 +1,55 @@
+export class Product {
+	private _id: string;
+	private _name: string;
+	private _price: number;
+
+
+	constructor(id: string, name: string, price: number) {
+		this._id = id;
+		this._name = name;
+		this._price = price;
+		this.validate();
+	}
+
+	validate() {
+		if (!this._id) {
+			throw new Error("Id is required");
+		}
+
+		if (!this._name) {
+			throw new Error("Name is required");
+		}
+
+		if (this._price <= 0) {
+			throw new Error("Price must be greater than zero");
+		}
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	get price() {
+		return this._price;
+	}
+
+	changeName(name: string) {
+		this._name = name;
+		this.validate();
+	}
+
+	changePrice(price: number) {
+		this._price = price;
+		this.validate();
+	}
+
+	applyDiscount(discountPercentage: number) {
+		const discount = this._price * discountPercentage / 100;
+		this._price = this._price - discount;
+		this.validate();
+	}
+}
