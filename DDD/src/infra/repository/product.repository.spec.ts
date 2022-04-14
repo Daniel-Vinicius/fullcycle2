0fae49ce98dc4690787e0e195de73e0715cc45c6
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-04-14 18:54:36 -0300

    Sequelize Setup

---
+++ DDD/src/infra/repository/product.repository.spec.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize-typescript";
+import { ProductModel } from "../database/sequelize/model/product.model";
+
+describe("Product repository test", () => {
+	let sequelize: Sequelize;
+
+	beforeEach(async () => {
+		sequelize = new Sequelize({
+			dialect: "sqlite",
+			storage: ":memory:",
+			logging: false,
+			sync: { force: true },
+		});
+
+		sequelize.addModels([ProductModel]);
+		await sequelize.sync();
+	});
+
+	afterEach(async () => {
+		await sequelize.close();
+	});
+
+});
