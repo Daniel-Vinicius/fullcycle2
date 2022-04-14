1ed808751a4f74f138145ad70f3244880dc3b3e1
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-04-14 18:28:39 -0300

    Entendendo camada de Infra

---
+++ DDD/src/domain/repository/productRepositoryInterface.ts
@@ -0,0 +1,5 @@
+import { RepositoryInterface } from "./repositoryInterface";
+import { Product } from "../entity/product";
+
+export type ProductRepositoryInterface = RepositoryInterface<Product>
+// export interface ProductRepositoryInterface extends RepositoryInterface<Product> {}
