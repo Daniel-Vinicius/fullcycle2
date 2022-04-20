dfedef5d128173ed1230b93267e2e2d217db5417
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-04-19 22:32:19 -0300

    Eventos de Customer

---
+++ DDD/src/domain/event/customer/customerChangedAddress.event.spec.ts
@@ -0,0 +1,43 @@
+import { EventDispatcher } from "@domain/event/@shared/eventDispatcher";
+
+import { CustomerChangedAddressEvent } from "./customerChangedAddress.event";
+import { SendConsoleLogAddressHandler } from "./handler/sendConsoleLogAddress.handler";
+
+describe("Customer changed of address event tests", () => {
+	it("should notify the event handlers of the change of address of a customer", () => {
+		const eventDispatcher = new EventDispatcher();
+		const eventHandler1 = new SendConsoleLogAddressHandler();
+
+		const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+
+		eventDispatcher.register("CustomerChangedAddressEvent", eventHandler1);
+
+		expect(eventDispatcher.eventHandlers["CustomerChangedAddressEvent"].length).toBe(1);
+
+		const eventPayload = {
+			customer: {
+				id: "123",
+				name: "John Doe",
+				oldAddress: {
+					street: "Old Street",
+					number: "123",
+					city: "Old City",
+					zip: "12345"
+				},
+				newAddress: {
+					street: "New Street",
+					number: "473",
+					city: "New City",
+					zip: "54321"
+				}
+			}			
+		};
+
+		const customerCreatedEvent = new CustomerChangedAddressEvent(eventPayload);
+
+		eventDispatcher.notify(customerCreatedEvent);
+
+		expect(spyEventHandler1).toHaveBeenCalled();
+	});
+
+});
