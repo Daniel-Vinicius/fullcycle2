a8297a3024ea153b329f6fc84a42384fab011192
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-06-14 17:45:31 -0300

    Implementando endpoint de listagem

--- cleanArchitecture/src/infra/api/routes/customer.route.ts
+++ cleanArchitecture/src/infra/api/routes/customer.route.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from "express";
 
 import { CustomerRepository } from "@infra/customer/repository/sequelize/customer.repository";
 
-import { CreateCustomerUseCase } from "@useCases/customer/create/create.customer.usecase";
 import { InputCreateCustomerDto } from "@useCases/customer/create/create.customer.dto";
+import { CreateCustomerUseCase } from "@useCases/customer/create/create.customer.usecase";
+import { ListCustomerUseCase } from "@useCases/customer/list/list.customer.usecase";
 
 export const customerRoute = express.Router();
 const repository = new CustomerRepository();
@@ -23,6 +24,18 @@ customerRoute.post("/", async (request: Request, response: Response) => {
 		const output = await usecase.execute(customerDto);
 
 		response.status(201).send(output);
+	} catch (error) {
+		response.status(400).send(error);
+	}
+});
+
+customerRoute.get("/", async (request: Request, response: Response) => {
+	const usecase = new ListCustomerUseCase(repository);
+
+	try {
+		const output = await usecase.execute({});
+
+		response.status(200).send(output);
 	} catch (error) {
 		response.status(400).send(error);
 	}
