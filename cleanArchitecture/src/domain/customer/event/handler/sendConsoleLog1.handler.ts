48a971ff8e1dc7127ae5d9140539d4c867d11c17
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-06-15 20:13:58 -0300

    comment console logs

--- cleanArchitecture/src/domain/customer/event/handler/sendConsoleLog1.handler.ts
+++ cleanArchitecture/src/domain/customer/event/handler/sendConsoleLog1.handler.ts
@@ -3,7 +3,7 @@ import { CustomerCreatedEvent } from "../customerCreated.event";
 
 export class SendConsoleLog1Handler implements EventHandlerInterface<CustomerCreatedEvent> {
 	handle(event: CustomerCreatedEvent): void {
-		console.log("This is the first console.log of the event CustomerCreatedEvent");
-		console.log(event);
+		// console.log("This is the first console.log of the event CustomerCreatedEvent");
+		// console.log(event);
 	}
 }
