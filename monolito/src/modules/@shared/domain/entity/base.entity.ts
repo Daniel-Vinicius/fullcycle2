497fe02445548ec6b8a3d76a45279a475a11aa1e
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-10-13 17:18:24 -0300

    Criando pacote shared

---
+++ monolito/src/modules/@shared/domain/entity/base.entity.ts
@@ -0,0 +1,29 @@
+import { Id } from "../value-object/id.value-object";
+
+export class BaseEntity {
+  private _id: Id;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+
+  constructor(id?: Id) {
+    this._id = id;
+    this._createdAt = new Date();
+    this._updatedAt = new Date();
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  get updatedAt() {
+    return this._updatedAt;
+  }
+
+  set updatedAt(updatedAt: Date) {
+    this._updatedAt = updatedAt;
+  }
+}
