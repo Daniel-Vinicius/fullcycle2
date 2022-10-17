445ca37ce5a1b80663d07fca5c2f81d6031507bb
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-10-17 15:12:02 -0300

    Implementando find a product

--- monolito/src/modules/product-adm/repository/product.repository.spec.ts
+++ monolito/src/modules/product-adm/repository/product.repository.spec.ts
@@ -51,4 +51,35 @@ describe("ProductRepository test", () => {
     expect(productDb.purchasePrice).toBe(productProps.purchasePrice);
     expect(productDb.stock).toBe(productProps.stock);
   });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const productProps = {
+      id: "1",
+      name: "My Product",
+      description: "Product description",
+      purchasePrice: 100,
+      stock: 10,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    ProductModel.create(productProps);
+
+    const result = await productRepository.find("1");
+
+    expect(result.id.id).toBe(productProps.id);
+    expect(result.name).toBe(productProps.name);
+    expect(result.description).toBe(productProps.description);
+    expect(result.purchasePrice).toBe(productProps.purchasePrice);
+    expect(result.stock).toBe(productProps.stock);
+  });
+
+  it("should throw error when product is not found", async () => {
+    const productRepository = new ProductRepository();
+
+    expect(async () => {
+      await productRepository.find("321");
+    }).rejects.toThrow("Product with 321 not found");
+  });
 });
--- monolito/src/modules/product-adm/repository/product.repository.ts
+++ monolito/src/modules/product-adm/repository/product.repository.ts
@@ -1,6 +1,21 @@
+import { Id } from "../../@shared/domain/value-object/id.value-object";
 import { Product } from "../domain/product.entity";
 import { ProductGateway } from "../gateway/product.gateway";
 import { ProductModel } from "./product.model";
+
+function productModelToProduct(productModel: ProductModel): Product {
+  const product = new Product({
+    id: new Id(productModel.id),
+    name: productModel.name,
+    description: productModel.description,
+    purchasePrice: productModel.purchasePrice,
+    stock: productModel.stock,
+    createdAt: productModel.createdAt,
+    updatedAt: productModel.updatedAt,
+  });
+
+  return product;
+}
 
 export class ProductRepository implements ProductGateway {
   async add(product: Product): Promise<void> {
@@ -15,7 +30,12 @@ export class ProductRepository implements ProductGateway {
     });
   }
 
-  find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+  async find(id: string): Promise<Product> {
+    try {
+      const productOnDB = await ProductModel.findByPk(id);
+      return productModelToProduct(productOnDB);
+    } catch (error) {
+      throw new Error(`Product with ${id} not found`);
+    }
   }
 }
