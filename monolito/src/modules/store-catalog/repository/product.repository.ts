aa17a5d2d1bcbc6a76878a6989665b8cbc9e5923
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-10-17 19:27:17 -0300

    ProductRepository store-catalog

---
+++ monolito/src/modules/store-catalog/repository/product.repository.ts
@@ -0,0 +1,26 @@
+import { Id } from "../../@shared/domain/value-object/id.value-object";
+import { Product } from "../domain/product.entity";
+import { ProductGateway } from "../gateway/product.gateway";
+import { ProductModel } from "./product.model";
+
+function productModelToProductEntity(productModel: ProductModel): Product {
+  const product = new Product({
+    id: new Id(productModel.id),
+    name: productModel.name,
+    description: productModel.description,
+    salesPrice: productModel.salesPrice,
+  });
+
+  return product;
+}
+
+export class ProductRepository implements ProductGateway {
+  async findAll(): Promise<Product[]> {
+    const products = await ProductModel.findAll();
+    return products.map((product) => productModelToProductEntity(product));
+  }
+
+  find(id: string): Promise<Product> {
+    throw new Error("Method not implemented.");
+  }
+}
