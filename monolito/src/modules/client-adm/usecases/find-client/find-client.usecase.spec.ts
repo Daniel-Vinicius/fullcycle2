2c2065a137e981d7491c240dc37dcc5877a93833
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-10-18 20:29:23 -0300

    Implementando FindClientUseCase

---
+++ monolito/src/modules/client-adm/usecases/find-client/find-client.usecase.spec.ts
@@ -0,0 +1,39 @@
+import { Id } from "../../../@shared/domain/value-object/id.value-object";
+import { Client } from "../../domain/client.entity";
+
+import { FindClientUseCase } from "./find-client.usecase";
+
+const client = new Client({
+  id: new Id("1"),
+  name: "Client 1",
+  email: "client1@email.com",
+  address: "Client 1 Address",
+});
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(client)),
+  };
+};
+
+describe("FindClientUseCase unit test", () => {
+  it("should find a client", async () => {
+    const repository = MockRepository();
+    const findClientUseCase = new FindClientUseCase(repository);
+
+    const input = {
+      id: "1",
+    };
+
+    const result = await findClientUseCase.execute(input);
+
+    expect(repository.find).toHaveBeenCalled();
+    expect(result.id).toEqual(input.id);
+    expect(result.name).toEqual(client.name);
+    expect(result.email).toEqual(client.email);
+    expect(result.address).toEqual(client.address);
+    expect(result.createdAt).toEqual(client.createdAt);
+    expect(result.updatedAt).toEqual(client.updatedAt);
+  });
+});
---
+++ monolito/src/modules/client-adm/usecases/find-client/find-client.usecase.ts
@@ -0,0 +1,28 @@
+import { UseCaseInterface } from "../../../@shared/usecase/usecase.interface";
+import { ClientGateway } from "../../gateway/client.gateway";
+
+import {
+  FindClientInputDto,
+  FindClientOutputDto,
+} from "./find-client.usecase.dto";
+
+export class FindClientUseCase implements UseCaseInterface {
+  private _clientRepository: ClientGateway;
+
+  constructor(clientRepository: ClientGateway) {
+    this._clientRepository = clientRepository;
+  }
+
+  async execute(input: FindClientInputDto): Promise<FindClientOutputDto> {
+    const client = await this._clientRepository.find(input.id);
+
+    return {
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
+    };
+  }
+}
