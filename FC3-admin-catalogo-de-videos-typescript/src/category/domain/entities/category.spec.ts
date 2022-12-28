630fb1a8a8a2f14eb7157966eb401c668c02e0c9
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-12-28 08:48:04 -0300

    Teste constructor da entidade categoria

--- FC3-admin-catalogo-de-videos-typescript/src/category/domain/entities/category.spec.ts
+++ FC3-admin-catalogo-de-videos-typescript/src/category/domain/entities/category.spec.ts
@@ -2,8 +2,23 @@ import { Category } from "./category";
 
 describe("Category Unit Tests", () => {
   test("constructor of category", () => {
-    const category = new Category({ name: "Movie" });
+    // Triple A - Arrange, Act, Assert
 
+    // Arrange
+    const props = {
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: new Date(),
+    };
+
+    // Act
+    const category = new Category(props);
+
+    // Assert
     expect(category.name).toBe("Movie");
+    expect(category.description).toBe("some description");
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBe(props.created_at);
   });
 });
