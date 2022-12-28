9574c14f0c8274cab90170b700700c5d8f9b2137
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-12-28 09:15:56 -0300

    Teste atribuição das propriedades facultativas

--- FC3-admin-catalogo-de-videos-typescript/src/category/domain/entities/category.spec.ts
+++ FC3-admin-catalogo-de-videos-typescript/src/category/domain/entities/category.spec.ts
@@ -1,24 +1,57 @@
 import { Category } from "./category";
+import { omit } from "lodash";
 
 describe("Category Unit Tests", () => {
   test("constructor of category", () => {
-    // Triple A - Arrange, Act, Assert
+    let category = new Category({ name: "Movie" });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
 
-    // Arrange
-    const props = {
+    let created_at = new Date();
+    category = new Category({
       name: "Movie",
       description: "some description",
+      is_active: false,
+      created_at,
+    });
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
       is_active: true,
-      created_at: new Date(),
-    };
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
 
-    // Act
-    const category = new Category(props);
-
-    // Assert
-    expect(category.name).toBe("Movie");
-    expect(category.description).toBe("some description");
-    expect(category.is_active).toBeTruthy();
-    expect(category.created_at).toBe(props.created_at);
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
   });
 });
--- FC3-admin-catalogo-de-videos-typescript/src/category/domain/entities/category.ts
+++ FC3-admin-catalogo-de-videos-typescript/src/category/domain/entities/category.ts
@@ -6,17 +6,30 @@ export type CategoryProperties = {
 };
 
 export class Category {
-  constructor(public readonly props: CategoryProperties) {}
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description;
+    this.is_active = this.props.is_active;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
 
   get name() {
     return this.props.name;
   }
+
   get description() {
     return this.props.description;
   }
 
+  private set description(value: string) {
+    this.props.description = value ?? null;
+  }
+
   get is_active() {
     return this.props.is_active;
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = value ?? true;
   }
 
   get created_at() {
