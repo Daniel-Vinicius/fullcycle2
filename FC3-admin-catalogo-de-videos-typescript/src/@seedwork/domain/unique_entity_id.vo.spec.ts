f3f6f70ad55b2a8662d52c58b02c7266cebe284d
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-12-28 11:12:55 -0300

    Teste Objeto de valor UniqueEntityId parte 1

---
+++ FC3-admin-catalogo-de-videos-typescript/src/@seedwork/domain/unique_entity_id.vo.spec.ts
@@ -0,0 +1,10 @@
+import { InvalidUuidError } from "@seedwork/errors/invalid_uuid.error";
+import { UniqueEntityId } from "./unique_entity_id.vo";
+
+describe("UniqueEntityId Unit Tests", () => {
+  it("should throw error when uuid is invalid", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
